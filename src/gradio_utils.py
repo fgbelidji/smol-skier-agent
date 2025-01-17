a6c1c6afe0d858694c93f4e9320f7338c064b0ea
fgbelidji <florent@huggingface.co>
2025-01-17 11:32:26 +0100

    Changed thinking streaming

--- src/gradio_utils.py
+++ src/gradio_utils.py
@@ -124,7 +124,7 @@ def interact_with_agent(agent, prompt, messages, df_routes, additional_args):
         reset_agent_memory=True,
         additional_args=additional_args,
     ):
-        if messages.metadata.get("title") == "Error 💥" or messages.metadata.get("title") == "🤔💭🔄" :
+        if messages.metadata.get("title", "") == "Error 💥" or messages.metadata.get("title", "") == "🤔💭🔄" :
             messages[-1] = msg
         else:
             messages.append(msg)
